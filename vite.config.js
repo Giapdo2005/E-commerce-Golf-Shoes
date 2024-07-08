9895af5af3def4f3e63a326bf287c9c81aef8137
Giapdo2005 <giapdo1901@gmail.com>
2024-07-07 21:10:28 -0700

    add: deploy workflows

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/E-commerce-Golf-Shoes/',
-  plugins: [react()]
+  plugins: [react()],
+  base: "/E-commerce-Golf-Shoes/"
 });
